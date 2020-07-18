25b8a1ebfa8e138036e6679c45ff7a8d6a43bb79
Serge <gavris.lov@gmail.com>
2020-07-18 10:25:40 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
+class App extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       name: "",
+       age: "",
+       hobbies: "",
+    }
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(`${this.state.name} ${this.state.age} ${this.state.hobbies}`);
+    this.setState({
+      name: "",
+       age: "",
+       hobbies: "",
+    });
+  };
+  
+  render() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form onSubmit={this.handleSubmit} >
+          <input type="text" placeholder="name" value={this.state.name} name="name" onChange={this.handleChange} />
+          <input type="text" placeholder="age" value={this.state.age} name="age" onChange={this.handleChange} />
+          <input type="text" placeholder="hobbies" value={this.state.hobbies} name="hobbies" onChange={this.handleChange} />
+          <button type="submit">Submit Form</button>
+        </form>
       </header>
     </div>
   );
+}
 }
 
 export default App;
